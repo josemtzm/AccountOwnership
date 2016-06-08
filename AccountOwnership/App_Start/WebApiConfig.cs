1a50b7f21b991f5a5704b728296d02e870b6dedd
Jose Martinez <josemtzm@me.com>
2016-06-08 17:15:19 -0500

    Se agrega SSL y se logra ejecutar la API en SharePoint

--- AccountOwnership/App_Start/WebApiConfig.cs
+++ AccountOwnership/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace AccountOwnership
 {
@@ -12,6 +13,9 @@ namespace AccountOwnership
     {
         public static void Register(HttpConfiguration config)
         {
+            //var cors = new EnableCorsAttribute("http://localhost:5555", "*", "*");
+            // enable  request for angularjs
+            //config.EnableCors(cors);
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
